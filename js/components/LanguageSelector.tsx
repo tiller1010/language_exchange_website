d0ef969696249635803eac3cb5bca0edeb1e4c24
Tyler Trout <tiller1010@gmail.com>
2022-08-20 09:32:39 -0400

    Reduced the number of available languages

--- js/components/LanguageSelector.tsx
+++ js/components/LanguageSelector.tsx
@@ -11,14 +11,49 @@ export default function LanguageSelector(props) {
 		required = false;
 	}
 
+	// Get all languages and format the selection array
+	let languagesArray = [];
+	languages.getLanguageCodes().forEach((langCode) => {
+		languagesArray.push({
+			languageName: languages.getLanguageName(langCode),
+			langCode,
+		});
+	});
+
+	// Filter for select languages
+	const validLanguages = [
+		'English',
+		'Spanish',
+		'French',
+		'German',
+		'Japanese',
+		'Korean',
+		'Russian',
+		'Chinese',
+		'Portugese',
+		'Swedish',
+		'Finnish',
+		'Arabic',
+		'Hebrew',
+	];
+	languagesArray = languagesArray.filter((lang) => validLanguages.indexOf(lang.languageName) != -1);
+
+	// Custom languages
+	languagesArray.push({
+		languageName: 'ASL',
+		langCode: 'ASL',
+	});
+
+	// Sort langauges by name
+	languagesArray = languagesArray.sort((a, b) => a.languageName.localeCompare(b.languageName));
+
 	return (
 		<div className="field dropdown">
 			<label htmlFor={id}>Language</label>
 			<select name={name} id={id} onChange={onChange} value={value} required={required}>
 				<option value="">Select a language</option>
-				<option>ASL</option>
-				{languages.getLanguageCodes().map((langCode) => 
-					<option key={langCode}>{languages.getLanguageName(langCode)}</option>
+				{languagesArray.map((lang) =>
+					<option key={lang.langCode}>{lang.languageName}</option>
 				)}
 			</select>
 		</div>
