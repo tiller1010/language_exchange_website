03ab83bcb0ac08dc665bfe539f3689403d754df2
Tyler <tiller1010@gmail.com>
2024-04-19 14:01:37 -0400

    fix: cipher with non-english characters

--- app/cipher.js
+++ app/cipher.js
@@ -2,11 +2,13 @@ function cipher(salt) {
     function textToChars(text) {
       return text.split('').map(function(c) { return c.charCodeAt(0); });
     }
+
     function byteHex(n) {
-      return ("0" + Number(n).toString(16)).substr(-2);
+      return '%' + Number(n).toString(16);
     }
+
     function applySaltToChar(code) {
-      return textToChars(salt).reduce(function(a,b) { return a ^ b; }, code);
+      return textToChars(salt).reduce(function(a, b) { return a ^ b; }, code);
     }
 
     return function(text) {
--- js/decipher.js
+++ js/decipher.js
@@ -1,8 +1,8 @@
 const decipher = salt => {
     const textToChars = text => text.split('').map(c => c.charCodeAt(0));
-    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
-    return encoded => encoded.match(/.{1,2}/g)
-      .map(hex => parseInt(hex, 16))
+    const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code);
+    return encoded => encoded.match(/%\w+(?=%)?/g)
+      .map(hex => parseInt(hex.substr((hex.length -1) * -1), 16))
       .map(applySaltToChar)
       .map(charCode => String.fromCharCode(charCode))
       .join('');
